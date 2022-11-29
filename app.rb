46720bd02333a40fa51df4e429909983e4d16034
ArBuZzZ44 <zahar1702@mail.ru>
2022-11-30 01:26:19 +0700

    add details.erb

--- app.rb
+++ app.rb
@@ -48,5 +48,8 @@ end
 get '/details/:id' do
 	 post_id = params[:id]
 
-	 erb "Displaying information for post with id #{post_id}"
+	 results = @db.execute 'select * from Posts where id = ?', [post_id]
+	 @row = results[0]
+
+	 erb :details
 end
