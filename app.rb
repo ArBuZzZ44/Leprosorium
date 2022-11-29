5f0a373fd04562890ca9ae326bff4161de848c87
ArBuZzZ44 <zahar1702@mail.ru>
2022-11-29 23:43:22 +0700

    Create table Posts

--- app.rb
+++ app.rb
@@ -2,6 +2,26 @@
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
+require 'sqlite3'
+
+def init_db
+	@db = SQLite3::Database.new 'leprosorium.db'
+	@db.results_as_hash = true
+end
+
+before do
+	init_db	
+end
+
+configure do
+	init_db
+	@db.execute 'CREATE TABLE if not exists Posts 
+		(
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			 created_date DATE,
+			  content TEXT
+		)' 
+end
 
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
